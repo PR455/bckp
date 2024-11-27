319be157008e5e45d06ac123ce350cbeadac6afb
PR455 <ibraprastya24@gmail.com>
2024-11-27 17:27:08 +0700

    Update create.php

--- create.php
+++ create.php
@@ -22,13 +22,36 @@ function formatArticle($article) {
     return '<p>' . $article . '</p>';
 }
 
-// Fungsi untuk menggantikan placeholders
-function replacePlaceholders($content, $replacements) {
-    return str_replace(
-        array_keys($replacements),
-        array_values($replacements),
-        $content
+function processContent($content, $replacements) {
+    // Ganti kurung kurawal ganda dengan kurung kurawal tunggal untuk proses awal
+    $processedContent = str_replace(['{{', '}}'], ['{', '}'], $content);
+    
+    // Ganti placeholder
+    $processedContent = str_replace(
+        [
+            '{BRAND_NAME}',
+            '{URL_PATH}',
+            '{AMP_URL}',
+            '{BRANDS_NAME}',
+            '{TITLE}',
+            '{DESCRIPTION}',
+            '{ARTICLE}',
+            '{ARTICLE_CONTENT}'
+        ],
+        [
+            $replacements['BRAND_NAME'],
+            $replacements['URL_PATH'],
+            $replacements['AMP_URL'],
+            $replacements['BRANDS_NAME'],
+            $replacements['TITLE'],
+            $replacements['DESCRIPTION'],
+            $replacements['ARTICLE'],
+            $replacements['ARTICLE_CONTENT']
+        ],
+        $processedContent
     );
+    
+    return $processedContent;
 }
 
 $filename = $gas_txt;
@@ -39,12 +62,40 @@ $titlesFile = $title_txt;
 $descriptionsFile = $descriptions_txt;
 $artikelFile = $artikel_txt;
 
-$titles = [];
-$descriptions = [];
-$articles = [];
-
 try {
-    // [Kode pembacaan file tetap sama...]
+    $titleContent = getFileContent($titlesFile);
+    $titles = array_filter(array_map('trim', explode("\n", $titleContent)));
+    
+    if (empty($titles)) {
+        throw new Exception("File title kosong atau tidak valid");
+    }
+    
+    $descriptionContent = getFileContent($descriptionsFile);
+    $descriptions = array_filter(array_map('trim', explode("\n", $descriptionContent)));
+    
+    if (empty($descriptions)) {
+        throw new Exception("File description kosong atau tidak valid");
+    }
+
+    $articleContent = getFileContent($artikelFile);
+    $articles = array_filter(array_map('trim', explode("\n", $articleContent)));
+    
+    if (empty($articles)) {
+        throw new Exception("File artikel kosong atau tidak valid");
+    }
+
+    $templateContent = getFileContent($templateFile);
+    $keywordsContent = getFileContent($filename);
+    $lines = array_filter(array_map('trim', explode("\n", $keywordsContent)));
+
+    if (!is_dir($mainDir)) {
+        if (!mkdir($mainDir, 0755, true)) {
+            throw new Exception("Gagal membuat direktori '$mainDir'");
+        }
+    }
+
+    $currentDomain = $_SERVER['HTTP_HOST'];
+    $titleIndex = $descriptionIndex = $articleIndex = 0;
 
     foreach ($lines as $line) {
         $folderName = str_replace(' ', '-', trim($line));
@@ -53,38 +104,36 @@ try {
         $folderURL = ensureTrailingSlash("https://$currentDomain/$folderName");
         $ampURL = ensureTrailingSlash("https://ampmasal.xyz/$folderName");
 
-        // Definisi replacements untuk semua placeholder
+        // Siapkan replacements
         $replacements = [
-            '{{BRAND_NAME}}' => strtoupper($folderName),
-            '{{URL_PATH}}' => $folderURL,
-            '{{AMP_URL}}' => $ampURL,
-            '{{BRANDS_NAME}}' => strtolower($folderName)
+            'BRAND_NAME' => strtoupper($folderName),
+            'URL_PATH' => $folderURL,
+            'AMP_URL' => $ampURL,
+            'BRANDS_NAME' => strtolower($folderName),
+            'TITLE' => '',
+            'DESCRIPTION' => '',
+            'ARTICLE' => '',
+            'ARTICLE_CONTENT' => ''
         ];
-        
-        // Proses title dengan placeholder
-        $title = isset($titles[$titleIndex]) ? replacePlaceholders($titles[$titleIndex], $replacements) : replacePlaceholders($titles[0], $replacements);
-        
-        // Proses description dengan placeholder
-        $description = isset($descriptions[$descriptionIndex]) ? replacePlaceholders($descriptions[$descriptionIndex], $replacements) : replacePlaceholders($descriptions[0], $replacements);
-        
-        // Proses article dengan placeholder
-        $article = isset($articles[$articleIndex]) ? formatArticle(replacePlaceholders($articles[$articleIndex], $replacements)) : formatArticle(replacePlaceholders($articles[0], $replacements));
 
-        // Update replacements dengan konten yang sudah diproses
-        $replacements['{{TITLE}}'] = $title;
-        $replacements['{{DESCRIPTION}}'] = $description;
-        $replacements['{{ARTICLE}}'] = $article;
-        $replacements['{{ARTICLE_CONTENT}}'] = $article;
+        // Proses konten dengan placeholder
+        $currentTitle = isset($titles[$titleIndex]) ? processContent($titles[$titleIndex], $replacements) : processContent($titles[0], $replacements);
+        $currentDescription = isset($descriptions[$descriptionIndex]) ? processContent($descriptions[$descriptionIndex], $replacements) : processContent($descriptions[0], $replacements);
+        $currentArticle = isset($articles[$articleIndex]) ? formatArticle(processContent($articles[$articleIndex], $replacements)) : formatArticle(processContent($articles[0], $replacements));
 
-        // Create directory if not exists
+        // Update replacements
+        $replacements['TITLE'] = $currentTitle;
+        $replacements['DESCRIPTION'] = $currentDescription;
+        $replacements['ARTICLE'] = $currentArticle;
+        $replacements['ARTICLE_CONTENT'] = $currentArticle;
+
         if (!is_dir($folderPath) && !mkdir($folderPath, 0755, true)) {
             continue;
         }
 
-        // Proses template dengan semua replacements
-        $customContent = replacePlaceholders($templateContent, $replacements);
+        // Proses template dengan replacements yang telah diupdate
+        $customContent = processContent($templateContent, $replacements);
 
-        // Write index.php
         $indexPath = "$folderPath/index.php";
         if (file_put_contents($indexPath, $customContent) !== false) {
             echo "🔗 <a href='$folderURL' target='_blank'>$folderURL</a><br>";
@@ -92,13 +141,12 @@ try {
             chmod($indexPath, 0644);
         }
 
-        // Update indices
         $titleIndex = ($titleIndex + 1) % count($titles);
         $descriptionIndex = ($descriptionIndex + 1) % count($descriptions);
         $articleIndex = ($articleIndex + 1) % count($articles);
     }
 
-    // [Kode .htaccess, sitemap.xml, dan robots.txt tetap sama...]
+    // [Kode untuk .htaccess, sitemap.xml, dan robots.txt tetap sama]
 
 } catch (Exception $e) {
     echo "<h2>Error:</h2>";
