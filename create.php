d69be3f75221d31da56d9d25f206e2d110119e9f
PR455 <ibraprastya24@gmail.com>
2024-11-28 12:42:47 +0700

    Update create.php

--- create.php
+++ create.php
@@ -51,12 +51,14 @@ function formatArticle($article) {
     return '<p>' . $article . '</p>';
 }
 
-function formatBrandName($brandName) {
-    // Ganti tanda hubung dengan spasi
-    return str_replace('-', ' ', $brandName);
-}
-
+// Fungsi asli - tidak diubah
 function processContent($content, $replacements) {
+    // Fungsi baru untuk memformat nama brand
+    function formatBrandName($brandName) {
+        // Ganti tanda hubung dengan spasi
+        return str_replace('-', ' ', $brandName);
+    }
+
     // Ganti kurung kurawal ganda dengan kurung kurawal tunggal untuk proses awal
     $processedContent = str_replace(['{{', '}}'], ['{', '}'], $content);
     
@@ -66,7 +68,6 @@ function processContent($content, $replacements) {
     // Ganti placeholder
     $processedContent = str_replace(
         [
-
             '{BRAND_NAME}',
             '{URL_PATH}',
             '{AMP_URL}',
